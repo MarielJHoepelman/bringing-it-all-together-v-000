3b82af433e161777feab619b6e84f2a40197cba4
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-11 20:40:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,12 +56,10 @@ attr_reader :id
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
-      dog = self.new(dog_data[0], dog_data[1], dog_data[2])
+      dog = Dog.new(id: result[0], name: result[1], breed: result[2])
     else
       dog = self.create(name: name, breed: breed)
     end
-    self
+    dog
   end
-=======
->>>>>>> fef7ce85e0ee6bfd5bc65fb1f4e7deb3c7b0da9f
 end
